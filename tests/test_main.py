839b182709c9c7c7514abd12d1e8e248e75f59d7
HOON <jihoon.jeong@glovis.mx>
2025-08-21 09:25:20 -0600

    hello to fastapi

--- tests/test_main.py
+++ tests/test_main.py
@@ -11,4 +11,4 @@ def tetst_hello_world(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.status_code != 500
-    assert response.json() == "Hello World!!"
+    assert response.json() == "FastAPI World!!"
